621c6b536d6ed455a924d5c6b31596277eda7f26
Oleg <frontendercode@gmail.com>
2020-03-02 19:08:46 +0200

    v.0.20
    Добавил блок FAQ

--- app/libs/common.js
+++ app/libs/common.js
@@ -1,21 +1,28 @@
-"use strict";
-$(document).ready(function () {
+'use strict';
+$(document).ready(function() {
+  $('.testi-slider').slick({
+    infinite: true,
+    dots: true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    //   autoplay       : true,
+    autoplaySpeed: 5000,
+    arrows: false,
+    prevArrow: "<button type='button' class='slick-prev pull-left'></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'></button>",
+  });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $('.item-header').click(function() {
+    $('.accordion-item').removeClass('active');
+    $(this)
+      .parent()
+      .addClass('active');
+    $('.icon')
+      .text('+')
+      .css('line-height', '25px');
+    $(this)
+      .children('.icon')
+      .text('-')
+      .css('line-height', '21px');
+  });
 });
